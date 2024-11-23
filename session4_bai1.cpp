872559826da028a5052be9fab36b9c0308ec6ae2
minh2307 <minh23923@gmail.com>
2024-11-23 23:11:07 +0700

    session4_bai1\

---
+++ session4_bai1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int number;
+	    printf("nhap mot so nguyen bat ky:");
+	    scanf("%d",&number);
+	    if( number > 0 ){
+	    	printf("%d la mot so nguyen duong",number);
+		}else{
+			printf("%d la mot so nguyen am",number);
+		}
+	
+return 0;
+}
